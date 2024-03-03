6817480a9ece466ae121dafc5577da0a8d050638
anatolhancharou <anatolhancharou@gmail.com>
2024-03-03 12:59:03 +0300

    feat: limit queries complexity

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { graphQLSchema } from './graphql-schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,6 +18,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
+
+      const errors = validate(graphQLSchema, parse(query), [depthLimit(5)]);
+
+      if (errors.length) {
+        return { errors };
+      }
 
       return graphql({
         schema: graphQLSchema,
